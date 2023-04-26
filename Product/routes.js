22a937b9b37f053e3c116da6f8f960fa581ee4ba
AshritaKumar <80836981+AshritaKumar@users.noreply.github.com>
2023-04-26 22:45:52 +0530

    Add files via upload

---
+++ Product/routes.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+
+//Mongoose
+const mongoose = require("mongoose");
+const Product = require("./product");
+
+//Connect to db
+mongoose
+  .connect(
+    "mongodb+srv://prasad:prasad@prasad.xg342ez.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log("Error : ", err);
+  });
+
+// Routes
+router.post("/addproduct", async (req, res) => {
+  const product = new Product({
+    pname: req.body.pname,
+    pquantity: req.body.pquantity,
+    pprice: req.body.pprice,
+  });
+  console.log(product);
+
+  const data = await product.save();
+
+  if (data) {
+    // console.log(data);
+    res.status(200).send("Success");
+  } else {
+    res.status(500).send("Error");
+  }
+});
+
+router.patch("/editproduct", async (req, res) => {
+  const data = await Product.findOneAndUpdate(
+    { _id: req.body._id },
+    {
+      $set: {
+        pname: req.body.pname,
+        pquantity: req.body.pquantity,
+        pprice: req.body.pprice,
+      },
+    }
+  );
+
+  if (data) {
+    res.status(200).send("Success");
+  } else {
+    res.status(500).send("Error");
+  }
+});
+
+router.delete("/deleteproduct/:_id", async (req, res) => {
+  const data = await Product.findByIdAndRemove(req.params._id);
+  if (data) {
+    res.status(200).send("Success");
+  } else {
+    res.status(500).send("Error");
+  }
+});
+
+router.get("/getproducts", async (req, res) => {
+  const data = await Product.find();
+
+  if (data) {
+    res.status(200).json(data);
+  } else {
+    res.status(500).send("Error");
+  }
+});
+
+module.exports = router;
